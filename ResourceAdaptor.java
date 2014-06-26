136417d8c59a0723379754f282a528fd6b9275b7
shao-you <s54088.cs98@g2.nctu.edu.tw>
2014-06-26 12:47:51 +0800

    Update ResourceAdaptor.java
    use Object Array to store 12 fields

--- ResourceAdaptor.java
+++ ResourceAdaptor.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -24,19 +25,21 @@ public class ResourceAdaptor {
 	    "  , passwd  VARCHAR(20))"; 
 
 	  //private String insertdbSQL = "insert into User(id,name,passwd) select ifNULL(max(id),0)+1,?,? FROM User"; 
-	  private String insertdbSQL = "insert into `Association`(`Association_ID`, `uid`, `in_port`, `sw_dpid`, `src_mac`, `dst_mac`, "+
+	  private String insertdb_Association = "insert into `Association` (`Association_ID`, `uid`, `in_port`, `sw_dpid`, `src_mac`, `dst_mac`, "+
 	  " `src_ip`, `dst_ip`, `src_port`, `dst_port`, `protocol`, `time`) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";//12 fields
+	  
+	  private String insert_Registered_mac = "INSERT INTO `Registered_mac` (`src_mac`, `time`, `idle`, `pass`) VALUES ('10:00:00:00:00:04', CURRENT_TIMESTAMP, '1', '0')";
+	  
 	  private String selectSQL = "SELECT * FROM `Association`"; 
-
+	  private String latest_record = "SELECT `uid` FROM `Association` where `src_mac`='10:00:00:00:00:03' ORDER BY `time` DESC LIMIT 1";
+	  
 	  public ResourceAdaptor() 
 	  {
 	    try { 
 	      Class.forName("com.mysql.jdbc.Driver"); 
 	      //註冊driver 
-	      String url1 = "jdbc:mysql://dbhome.cs.nctu.edu.tw/wusy_cs_ttt?"
-	    		  + "useUnicode=true&characterEncoding=Big5";
-	      String url2 = "jdbc:mysql://140.113.215.4/test?"
-	    		  + "useUnicode=true&characterEncoding=Big5";
+	      //String url1 = "jdbc:mysql://dbhome.cs.nctu.edu.tw/wusy_cs_ttt?useUnicode=true&characterEncoding=Big5";
+	      String url2 = "jdbc:mysql://140.113.215.4/test?useUnicode=true&characterEncoding=Big5";
 	      //con = DriverManager.getConnection(url1,"wusy_cs","*****"); 
 	      con = DriverManager.getConnection(url2,"dlink","123456"); 
 	      //取得connection
@@ -76,13 +79,13 @@ public class ResourceAdaptor {
 	  } 
 	  //新增資料 
 	  //可以看看PrepareStatement的使用方式 
-	  public void insertTable(String Association_ID, String uid, short in_port, long sw_dpid, String src_mac, String dst_mac, String src_ip, String dst_ip, 
-			  int src_port, int dst_port, byte protocol, java.sql.Timestamp time) 
+	  public void insertTable(int Association_ID, String uid, short in_port, long sw_dpid, String src_mac, String dst_mac, String src_ip, String dst_ip, 
+			  short src_port, short dst_port, byte protocol, java.sql.Timestamp time) 
 	  { 
 	    try 
 	    { 
-	      pst = con.prepareStatement(insertdbSQL); 
-	      pst.setString(1, Association_ID);
+	      pst = con.prepareStatement(insertdb_Association); 
+	      pst.setInt(1, Association_ID);
 	      pst.setString(2, uid);
 	      pst.setShort(3, in_port);
 	      pst.setLong(4, sw_dpid);
@@ -90,8 +93,8 @@ public class ResourceAdaptor {
 	      pst.setString(6, dst_mac);
 	      pst.setString(7, src_ip);
 	      pst.setString(8, dst_ip);
-	      pst.setInt(9, src_port);
-	      pst.setInt(10, dst_port);
+	      pst.setShort(9, src_port);
+	      pst.setShort(10, dst_port);
 	      pst.setByte(11, protocol);
 	      pst.setTimestamp(12, time);
 	      
@@ -108,7 +111,7 @@ public class ResourceAdaptor {
 	  } 
 	  //刪除Table, 
 	  //跟建立table很像 
-	  public void dropTable() 
+	  private void dropTable()//dangerous 
 	  { 
 	    try 
 	    { 
@@ -126,20 +129,41 @@ public class ResourceAdaptor {
 	  } 
 	  //查詢資料 
 	  //可以看看回傳結果集及取得資料方式 
-	  public void SelectTable() 
+	  public Object [] SelectTable(String command)//only query a record, return array of object
 	  { 
-	    try 
+		  Object [] meatadata = new Object[12];
+		try 
 	    { 
 	      stat = con.createStatement(); 
-	      rs = stat.executeQuery(selectSQL); 
-    	  //System.out.println(rs);
+	      rs = stat.executeQuery(command); 
+    	  System.out.println(command+": ");
 	      System.out.println("Association_ID     uid     in_port     sw_dpid     src_mac     dst_mac     src_ip     dst_ip     src_port     dst_port     protocol     time");
-	      while(rs.next()) 
+	      
+	      //ResultSetMetaData rsmd = rs.getMetaData();
+	      //int columnsNumber = rsmd.getColumnCount();
+	      while(rs.next())
 	      { 
-	    	  System.out.println(rs.getString("Association_ID")+"\t"+rs.getString("uid")+"\t"+rs.getShort("in_port")
-	    			  +"\t"+rs.getLong("sw_dpid")+"\t"+rs.getString("src_mac")+"\t"+rs.getString("dst_mac")
-	    			  +"\t"+rs.getString("src_ip")+"\t"+rs.getString("dst_ip")+"\t"+rs.getInt("src_port")+"\t"+rs.getInt("dst_port")
-	    			  +"\t"+rs.getByte("protocol")+"\t"+rs.getTimestamp("time"));       
+	    	  /*for (int i = 1; i <= columnsNumber; i++)
+	    	  {
+	    		  String columnValue = rs.getString(i);
+	              System.out.println(columnValue + " " + rsmd.getColumnName(i));
+	    	  }*/
+	    	  meatadata[0] = rs.getInt("Association_ID");
+	    	  meatadata[1] = rs.getString("uid");
+	    	  meatadata[2] = rs.getShort("in_port");
+	    	  meatadata[3] = rs.getLong("sw_dpid");
+	    	  meatadata[4] = rs.getString("src_mac");
+	    	  meatadata[5] = rs.getString("dst_mac");
+	    	  meatadata[6] = rs.getString("src_ip");
+	    	  meatadata[7] = rs.getString("dst_ip");
+	    	  meatadata[8] = rs.getShort("src_port");
+	    	  meatadata[9] = rs.getShort("dst_port");
+	    	  meatadata[10] = rs.getByte("protocol");
+	    	  meatadata[11] = rs.getTimestamp("time");
+	    	  //System.out.println(rs.getInt("Association_ID")+"\t"+rs.getString("uid")+"\t"+rs.getShort("in_port")
+	    			  //+"\t"+rs.getLong("sw_dpid")+"\t"+rs.getString("src_mac")+"\t"+rs.getString("dst_mac")
+	    			  //+"\t"+rs.getString("src_ip")+"\t"+rs.getString("dst_ip")+"\t"+rs.getShort("src_port")+"\t"+rs.getShort("dst_port")
+	    			  //+"\t"+rs.getByte("protocol")+"\t"+rs.getTimestamp("time"));
 	      }
 	    } 
 	    catch(SQLException e) 
@@ -150,6 +174,7 @@ public class ResourceAdaptor {
 	    { 
 	      Close(); 
 	    } 
+		return meatadata;
 	  } 
 	  //完整使用完資料庫後,記得要關閉所有Object 
 	  //否則在等待Timeout時,可能會有Connection poor的狀況 
@@ -179,7 +204,7 @@ public class ResourceAdaptor {
 	    } 
 	  } 
 
-	public static void main(String[] args) 
+	/*public static void main(String[] args) 
 	  { 
 	      //測看看是否正常 
 		  java.util.Date date = new java.util.Date();
@@ -196,8 +221,7 @@ public class ResourceAdaptor {
 	      ResourceAdaptor test = new ResourceAdaptor(); 
 		  //test.dropTable();
 		  //test.createTable();//no use
-		  test.SelectTable();
-		  //test.insertTable("hashcode2", "Alex", (short)3, (long)15, "10:00:00:00:00:02", "10:00:00:00:00:03", "140.113.215.4", "8.8.8.8", (int)17, (int)6, (byte)4, sqlTimestamp);
-		  test.Close();
-	  } 
+		  test.SelectTable(selectSQL);
+		  //test.insertTable((int)7788, "Alex", (short)3, (long)15, "10:00:00:00:00:02", "10:00:00:00:00:03", "140.113.215.4", "8.8.8.8", (short)17, (short)6, (byte)4, sqlTimestamp);
+	  }*/ 
 }
