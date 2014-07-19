090a1db8a56d09b62bd65fd133930da640d6d2b3
shao-you <s54088.cs98@g2.nctu.edu.tw>
2014-07-19 19:22:26 +0800

    Update SocketServer.java
    receive the whole metadata

--- ID-Based Management System/SocketServer.java
+++ ID-Based Management System/SocketServer.java
@@ -1,15 +1,23 @@
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
- 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SocketServer extends java.lang.Thread {
  
     private boolean OutServer = false;
     private ServerSocket server;
     private final int ServerPort = 8765;// 要監控的port
- 
-    public SocketServer() {
+    Dispatcher dispatcher;
+    
+    public SocketServer(Dispatcher dispatcher) {
         try {
             server = new ServerSocket(ServerPort);
+            this.dispatcher = dispatcher;
  
         } catch (java.io.IOException e) {
             System.out.println("Socket啟動有問題 !");
@@ -19,7 +27,8 @@ public class SocketServer extends java.lang.Thread {
  
     public void run() {
         Socket socket;
-        java.io.BufferedInputStream in;
+        //java.io.ObjectInputStream in ;//receive object
+        java.io.BufferedInputStream in;//receive string
  
         System.out.println("伺服器已啟動 !");
         while (!OutServer) {
@@ -33,31 +42,42 @@ public class SocketServer extends java.lang.Thread {
                 // TimeOut時間
                 socket.setSoTimeout(15000);
  
+                /*in = new java.io.ObjectInputStream(socket.getInputStream());
+                Asso_record record = (Asso_record) in.readObject();//restore to JSON format, class Asso_record位置(package): 送端要等於收端
+                record.Printout_record();*/
+                
                 in = new java.io.BufferedInputStream(socket.getInputStream());
                 byte[] b = new byte[1024];
+                
                 String data = "";
                 int length;
                 while ((length = in.read(b)) > 0)// <=0的話就是結束了
                 {
                     data += new String(b, 0, length);
                 }
- 
-                System.out.println("我取得的值:" + data);
+                JSONObject record_json = new JSONObject(data);
+                System.out.println(record_json);
+                //System.out.println("我取得的值:" + data);
+                this.dispatcher.trigger(record_json);
+                
+                //String record_of_Association_ID = "SELECT * FROM `Association` where `Association_ID`='"+ data +"'";
+                //ResourceAdaptor DB_manipulate = new ResourceAdaptor();
+                //Object [] result = DB_manipulate.SelectTable(record_of_Association_ID);
+                
+                
+                //do some simple handling of IDs (notification, classification, register/publish...)
+                
                 in.close();
                 in = null;
-                
-                String record_of_Association_ID = "SELECT * FROM `Association` where `Association_ID`='"+ data +"'";
-                ResourceAdaptor DB_manipulate = new ResourceAdaptor();
-                Object [] result = DB_manipulate.SelectTable(record_of_Association_ID);
-                //do some simple handling of IDs (notification, classification, regist/publish...)
-                
-                
                 socket.close();
  
             } catch (java.io.IOException e) {
                 System.out.println("Socket連線有問題 !");
                 System.out.println("IOException :" + e.toString());
-            }
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
  
         }
     }
