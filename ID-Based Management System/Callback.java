4cf349dfde6a1db0fb410942659faa477c4b7a04
shao-you <s54088.cs98@g2.nctu.edu.tw>
2014-07-19 19:17:41 +0800

    Create Callback.java
    define Callback interface

---
+++ ID-Based Management System/Callback.java
@@ -0,0 +1,7 @@
+
+public interface Callback {  
+	final int record_field = 12;
+	int [] register_index = new int [record_field];
+	//for(int i=0;i<register_index;i++);
+    public void policy_action(String result);  
+}  
