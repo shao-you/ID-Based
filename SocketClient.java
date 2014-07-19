4b50a111ec349f33f29cb250d5dfe6ebd5d25492
shao-you <s54088.cs98@g2.nctu.edu.tw>
2014-07-19 19:25:33 +0800

    Update SocketClient.java
    transmit json file

--- SocketClient.java
+++ SocketClient.java
@@ -3,24 +3,38 @@ package net.floodlightcontroller.headerextract;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.io.BufferedOutputStream;
- 
+import java.io.ObjectOutputStream;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SocketClient {
     private String address = "140.113.215.4";// 連線的ip
     private int port = 8765;// 連線的port
  
     public SocketClient() {
-    	this(99999);
+    	//this(99999);
     }
-    public SocketClient(int Association_ID) {
+    public SocketClient(JSONObject record) {
  
         Socket client = new Socket();
         InetSocketAddress isa = new InetSocketAddress(this.address, this.port);
         try {
+        	
+        	/*client.connect(isa,10000);
+            ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
+            //送出object
+            out.writeObject(record);
+            out.flush();
+            out.close();
+            out = null ;
+            client.close();
+            client = null ;*/
+
             client.connect(isa, 10000);
-            BufferedOutputStream out = new BufferedOutputStream(client
-                    .getOutputStream());
+            BufferedOutputStream out = new BufferedOutputStream(client.getOutputStream());
             // 送出字串
-            String msg = Integer.toString(Association_ID);
+            String msg = record.toString();
             out.write(msg.getBytes());
             out.flush();
             out.close();
