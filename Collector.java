e54446b46f687f9c0a09426f7af9c6d56e0c299a
shao-you <s54088.cs98@g2.nctu.edu.tw>
2014-07-26 19:16:27 +0800

    Update Collector.java
    add packet_out method (but seems to have problems) & set isCallbackOrderingPostreq

--- Collector.java
+++ Collector.java
@@ -1,10 +1,12 @@
 package net.floodlightcontroller.headerextract;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,12 +15,19 @@ import org.json.JSONObject;
 import org.openflow.protocol.OFMatch;
 import org.openflow.protocol.OFMessage;
 import org.openflow.protocol.OFPacketIn;
+import org.openflow.protocol.OFPacketOut;
 import org.openflow.protocol.OFType;
+import org.openflow.protocol.action.OFAction;
+import org.openflow.protocol.action.OFActionDataLayerDestination;
+import org.openflow.protocol.action.OFActionNetworkLayerDestination;
+import org.openflow.protocol.action.OFActionOutput;
+import org.openflow.protocol.action.OFActionTransportLayerDestination;
 import org.openflow.util.HexString;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
  
 import net.floodlightcontroller.routing.IRoutingService;
+import net.floodlightcontroller.routing.Route;
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.IOFMessageListener;
@@ -34,13 +43,15 @@ import net.floodlightcontroller.devicemanager.SwitchPort;
 import net.floodlightcontroller.learningswitch.LearningSwitch;
 import net.floodlightcontroller.packet.Ethernet;
 import net.floodlightcontroller.packet.IPv4;
-import net.floodlightcontroller.topology.ITopologyService;
-
+//import net.floodlightcontroller.topology.ITopologyService;
 
 public class HeaderExtract implements IOFMessageListener, IFloodlightModule {
  
 	public final int DEFAULT_CACHE_SIZE = 10;
 	protected IFloodlightProviderService floodlightProvider;
+	protected ICounterStoreService counterStore;
+	protected IDeviceService deviceManager;
+	protected IRoutingService routingEngine;
 
 	protected static Logger log = LoggerFactory.getLogger(LearningSwitch.class);
 	ResourceAdaptor DB_manipulate;
@@ -60,14 +71,13 @@ public class HeaderExtract implements IOFMessageListener, IFloodlightModule {
 	@Override
 	public boolean isCallbackOrderingPostreq(OFType type, String name) {
 		// TODO Auto-generated method stub
-		return false;
+		return (type.equals(OFType.PACKET_IN) && name.equals("forwarding"));//比forwarding先處理pkt_in
+		//return false;
 	}
  
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
 		// TODO Auto-generated method stub
- 
- 
         return null;
 	}
  
@@ -80,11 +90,11 @@ public class HeaderExtract implements IOFMessageListener, IFloodlightModule {
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
 		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
-		 l.add(IFloodlightProviderService.class);
+		    l.add(IFloodlightProviderService.class);
+		    l.add(ICounterStoreService.class);
+		    l.add(IRoutingService.class);
 	        l.add(IDeviceService.class);
-	        l.add(IRoutingService.class);
-	        l.add(ITopologyService.class);
-	        l.add(ICounterStoreService.class);
+	        //l.add(ITopologyService.class);	        
 		return l;
 		// TODO Auto-generated method stub
 	}
@@ -92,9 +102,11 @@ public class HeaderExtract implements IOFMessageListener, IFloodlightModule {
 	@Override
 	public void init(FloodlightModuleContext context)
 			throws FloodlightModuleException {
-		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		this.floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		this.counterStore = context.getServiceImpl(ICounterStoreService.class);
+		this.routingEngine = context.getServiceImpl(IRoutingService.class);
+		this.deviceManager = context.getServiceImpl(IDeviceService.class);
 		// TODO Auto-generated method stub
- 
 	}
  
 	@Override
@@ -140,8 +152,11 @@ public class HeaderExtract implements IOFMessageListener, IFloodlightModule {
 	        	case PACKET_IN:
 		
 				time = new java.sql.Timestamp(t);
-				IDevice srcDevice = IDeviceService.fcStore.get(cntx, IDeviceService.CONTEXT_SRC_DEVICE);
-			    SwitchPort[] srcDaps = srcDevice.getAttachmentPoints();//Get all unique attachment points associated with the device.
+			    IDevice srcDevice = IDeviceService.fcStore.get(cntx, IDeviceService.CONTEXT_SRC_DEVICE);
+				IDevice dstDevice = IDeviceService.fcStore.get(cntx, IDeviceService.CONTEXT_DST_DEVICE);
+				SwitchPort[] srcDaps = srcDevice.getAttachmentPoints();//Get all unique attachment points associated with the device.
+				SwitchPort[] dstDaps = dstDevice.getAttachmentPoints();
+			    
 			    if(srcDaps.length==0) System.out.println("=========================");
 			    else in_port = (short)srcDaps[0].getPort();
 			    sw_dpid = srcDaps[0].getSwitchDPID();
@@ -166,6 +181,7 @@ public class HeaderExtract implements IOFMessageListener, IFloodlightModule {
 				else System.out.println("SUCCESSFUL CONNECTION!!");
 				
 				String latest_record = "SELECT * FROM `Association` where `src_mac`='"+ src_mac +"' ORDER BY `time` DESC LIMIT 1";//for `uid`
+				//http://www.mysqltutorial.org/mysql-limit.aspx
 				String mac_idle_pass = "SELECT * FROM `Registered_mac` where `src_mac`='"+ src_mac +"'";//for `pass`
 				
 				//query Association table by scr_mac, and find the latest record, and copy uid into new record 
@@ -205,10 +221,17 @@ public class HeaderExtract implements IOFMessageListener, IFloodlightModule {
 					JSONObject record_json=null;
 					record_json = new JSONObject(map);
 					System.out.println(record_json);
-					new SocketClient(record_json);
+					SocketClient client = new SocketClient();
+					client.connect_server(record_json);//will wait for the upper layer to complete
+					
 				}
-				//=============================================================
-				//=============================================================
+				
+				Route route = 
+						routingEngine.getRoute(srcDaps[0].getSwitchDPID(), (short)srcDaps[0].getPort(),
+								dstDaps[0].getSwitchDPID(), (short)dstDaps[0].getPort(), 0); 
+				short first_DPID_port = route.getPath().get(1).getPortId();
+				//pushPacket(sw, match, pin, first_DPID_port);//pkt_out the first pkt buffered in the first switch
+				//return Command.STOP;//policy engine阻擋, 否則交由forwarding
 				//=============================================================
 				/*Long sourceMACHash = Ethernet.toLong(match.getDataLayerDestination());
 				System.out.println("$$$$$-Get the Destination IP Address-$$$$$"); 
@@ -218,9 +241,83 @@ public class HeaderExtract implements IOFMessageListener, IFloodlightModule {
 				
 		        default:
 		            break;
-			}
-		}
+			}//end switch
+		}//end if
 		return Command.CONTINUE;
 		//return Command.STOP;
 	}
+	
+	private void pushPacket(IOFSwitch sw, OFMatch match, OFPacketIn pi, short outport) {
+	    if (pi == null) {
+	        return;
+	    }
+
+	    // The assumption here is (sw) is the switch that generated the
+	    // packet-in. If the input port is the same as output port, then
+	    // the packet-out should be ignored.
+	    if (pi.getInPort() == outport) {
+	        if (log.isDebugEnabled()) {
+	            log.debug("Attempting to do packet-out to the same " +
+	                      "interface as packet-in. Dropping packet. " +
+	                      " SrcSwitch={}, match = {}, pi={}",
+	                      new Object[]{sw, match, pi});
+	            return;
+	        }
+	    }
+
+	    if (log.isTraceEnabled()) {
+	        log.trace("PacketOut srcSwitch={} match={} pi={}",
+	                  new Object[] {sw, match, pi});
+	    }
+
+	    OFPacketOut po =
+	            (OFPacketOut) floodlightProvider.getOFMessageFactory()
+	                                            .getMessage(OFType.PACKET_OUT);
+
+	    // set actions
+	    List<OFAction> actions = new ArrayList<OFAction>();
+	    actions.add(new OFActionOutput(outport, (short) 0xffff));
+		//actions.add(new OFActionDataLayerDestination(Ethernet.toMACAddress(mac_10_0_0_1)));
+		//actions.add(new OFActionNetworkLayerDestination(IPv4.toIPv4Address("10.0.0.1")));
+		//actions.add(new OFActionTransportLayerDestination((short)5134));
+		int length = OFActionOutput.MINIMUM_LENGTH            		
+		       // + OFActionNetworkLayerDestination.MINIMUM_LENGTH 
+	           // + OFActionDataLayerDestination.MINIMUM_LENGTH
+	           // + OFActionTransportLayerDestination.MINIMUM_LENGTH
+				;
+	    po.setActions(actions)
+	      .setActionsLength((short)length);
+	    short poLength =
+	            (short) (po.getActionsLength() + OFPacketOut.MINIMUM_LENGTH);
+
+	    // If the switch doens't support buffering set the buffer id to be none
+	    // otherwise it'll be the the buffer id of the PacketIn
+	    if (sw.getBuffers() == 0) {
+	        // We set the PI buffer id here so we don't have to check again below
+	        pi.setBufferId(OFPacketOut.BUFFER_ID_NONE);
+	        po.setBufferId(OFPacketOut.BUFFER_ID_NONE);
+	    } else {
+	        po.setBufferId(pi.getBufferId());
+	    }
+
+	    po.setInPort(pi.getInPort());
+
+	    // If the buffer id is none or the switch doesn's support buffering
+	    // we send the data with the packet out
+	    if (pi.getBufferId() == OFPacketOut.BUFFER_ID_NONE) {
+	        byte[] packetData = pi.getPacketData();
+	        poLength += packetData.length;
+	        po.setPacketData(packetData);
+	    }
+
+	    po.setLength(poLength);
+
+	    try {
+	        counterStore.updatePktOutFMCounterStoreLocal(sw, po);
+	        sw.write(po, null);
+	    } catch (IOException e) {
+	        log.error("Failure writing packet out", e);
+	    }
+	}
+	
 }
