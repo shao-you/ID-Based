7bc1fce3352855fa3e64179febd9b275a779f5cc
shao-you <s54088.cs98@g2.nctu.edu.tw>
2014-07-26 19:51:39 +0800

    Update SocketClient.java
    send signal (true/false) back to controller

--- SocketClient.java
+++ SocketClient.java
@@ -2,7 +2,11 @@ package net.floodlightcontroller.headerextract;
 
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,34 +15,66 @@ import org.json.JSONObject;
 public class SocketClient {
     private String address = "140.113.215.4";// 連線的ip
     private int port = 8765;// 連線的port
+    private Socket client;
+    private InetSocketAddress isa;
+    private Boolean forwarding_or_not = true;
  
     public SocketClient() {
-    	//this(99999);
+    	client  = new Socket();
+    	isa = new InetSocketAddress(this.address, this.port);
     }
-    public SocketClient(JSONObject record) {
- 
-        Socket client = new Socket();
-        InetSocketAddress isa = new InetSocketAddress(this.address, this.port);
+    public Boolean forwarding_or_not()
+    {
+    	return forwarding_or_not;
+    }
+    public void connect_server(JSONObject record) {
+
         try {
+        	client.connect(isa, 10000);
         	
         	/*client.connect(isa,10000);
             ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
-            //送出object
+            // 送出object
             out.writeObject(record);
             out.flush();
             out.close();
             out = null ;
             client.close();
             client = null ;*/
-
-            client.connect(isa, 10000);
-            BufferedOutputStream out = new BufferedOutputStream(client.getOutputStream());
+            
+            /*BufferedOutputStream out = new BufferedOutputStream(client.getOutputStream());
             // 送出字串
             String msg = record.toString();
             out.write(msg.getBytes());
             out.flush();
-            out.close();
-            out = null;
+            //out.close();
+            //out = null;*/
+        	
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+			out.writeUTF(record.toString());
+			out.flush();
+			
+           /*BufferedInputStream in = new BufferedInputStream(client.getInputStream());
+           // 接收字串
+            byte[] b = new byte[1024];
+            String data = "";
+            int length;
+            while ((length = in.read(b)) > 0)// <=0的話就是結束了
+            {
+                data += new String(b, 0, length);
+            }
+            System.out.println(data);//"Action complete!!"
+            //in.close();
+            //in = null;*/
+            
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            
+            String result = in.readUTF(); 
+            if(result.equals("true")) forwarding_or_not = true;
+            else forwarding_or_not = false;
+            
+            System.out.println("Server："+result);
+            
             client.close();
             client = null;
  
