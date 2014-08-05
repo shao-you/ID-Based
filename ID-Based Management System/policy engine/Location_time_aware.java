8ecdfab5b0e81e16a773a7c367afee8793cc9584
shao-you <s54088.cs98@g2.nctu.edu.tw>
2014-08-05 16:58:02 +0800

    Update Location_time_aware.java
    able to set rules to the switch

--- ID-Based Management System/policy engine/Location_time_aware.java
+++ ID-Based Management System/policy engine/Location_time_aware.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,25 +20,57 @@ public class Location_time_aware implements Callback {
 		this.register.Register_related_ID(Location_time_aware.this, register_index);//register interested IDs
 	}
 	@Override  
-    public Boolean policy_action(final String result) {  //do the checking & set rules
+    public Boolean policy_action(final String metadata) {  //do the checking & set rules
 		//restore JSON record
+		JSONObject record_json=null;
+
 		try {
-			JSONObject record_json = new JSONObject(result);
-		} catch (JSONException e) {
+			record_json = new JSONObject(metadata);
+		} catch (JSONException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
-		System.out.println(result);
+		System.out.println(metadata);
+		
+		//read the external list
 		
 		//decide the intelligent
-		Boolean forwarding_or_not = true;
-		
+		Boolean forwarding_or_not = false;//need to set rules, forwarding should stop
+
 		//set rules
-		if(forwarding_or_not)
+		if(forwarding_or_not == false)
 		{
-			//Flow flow = new Flow();
+			Flow flow = new Flow();
+			Match match = new Match();
+			ArrayList<Action>actions = new ArrayList<Action>();
+			
+			try {
+				String src_mac = (String)record_json.get("src_mac");
+				String sw_dpid = (String)record_json.get("sw_dpid");
+				
+				match.setDataLayerSource(src_mac);//use src_mac to block 
+				flow.setSwitch(sw_dpid);
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			//actions.add(new Action("output", "1"));//no action will get the flow dropped
+		    flow.setIdleTimeOut("0");//0 means infinite, in seconds
+		    flow.setHardTimeOut("0");
+		    
+			flow.setMatch(match);
+			flow.setActions(actions);
+			flow.setName( Integer.toString(new Random().nextInt()) );//an unique name
+			
+			String result;
+			try {
+				result = REST_Commander.push(flow);
+				System.out.println(result);
+			} catch (IOException | JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}/**/
 		}
-		
 		
 		/*Thread thread = new Thread(new Runnable() {  
             @Override  
