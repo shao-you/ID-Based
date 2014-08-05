53c47f946bf96eafdd2dbffd92606fbfa09972f7
shao-you <s54088.cs98@g2.nctu.edu.tw>
2014-08-05 10:08:54 +0800

    Update Register.java

--- ID-Based Management System/Register.java
+++ ID-Based Management System/Register.java
@@ -1,20 +1,17 @@
 
 public class Register {
-	int number_of_policy_engine;
-	//Boolean forwarding_or_not;
 	int [][] array;
 	int index = 0;
-	final int record_field = 12;
+	int record_field;
 	Callback [] callback;
-	//JSONObject record_json;
 	
-	public Register(int num)
+	public Register(int policy_engine_num, int record_field_num)
 	{
-		number_of_policy_engine = num;
-		array = new int [num][record_field];
-		callback = new Callback[num];
+		record_field = record_field_num;
+		array = new int [policy_engine_num][record_field];
+		callback = new Callback[policy_engine_num];
 	}
-	public void Register_related_ID(Callback callback, int [] register_index)//store the registered info.
+	public void Register_related_ID(Callback callback, int [] register_index)//store the registered info. for each policy engine
 	{  
         for(int i=0;i<record_field;i++) array[index][i] = register_index[i];
 		this.callback[index] = callback;
