d7e427bbe6f742fd6c6cc7ddb3def55e7dc14053
shao-you <s54088.cs98@g2.nctu.edu.tw>
2014-08-09 17:27:56 +0800

    Create APT_detection.java
    APT_detection policy engine prototype

---
+++ ID-Based Management System/policy engine/APT_detection.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class APT_detection implements Callback {
+	private Register register;
+	//final private String uid;
+	//final private java.sql.Timestamp time;
+	//final private long sw_dpid;
+	
+	@Override
+	public void register_IDs(Register register) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Boolean policy_action(String metadata) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
