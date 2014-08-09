58e4604e5f5347c81b142edd63711d8d5df3b789
shao-you <s54088.cs98@g2.nctu.edu.tw>
2014-08-09 17:30:25 +0800

    Update Callback.java
    add an interface method "register_IDs"

--- ID-Based Management System/Callback.java
+++ ID-Based Management System/Callback.java
@@ -3,5 +3,6 @@ public interface Callback {
 	final int record_field = 12;
 	int [] register_index = new int [record_field];
 	//for(int i=0;i<register_index;i++);
-    public Boolean policy_action(String result);
+	public void register_IDs(Register register);
+    public Boolean policy_action(String metadata);
 }  
